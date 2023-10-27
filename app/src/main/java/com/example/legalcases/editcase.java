84864e45382ff8262dc2abddaee12fa445cd0c10
Shreya <thisisshreya1998@gmail.com>
2023-10-27 17:30:55 +0530

    27-10

---
+++ app/src/main/java/com/example/legalcases/editcase.java
@@ -0,0 +1,14 @@
+package com.example.legalcases;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class editcase extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editcase);
+    }
+}
