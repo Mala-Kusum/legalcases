4a957bac6ecdd6aa836ae6ae9b6572a7308e8cf5
Shreya <thisisshreya1998@gmail.com>
2023-11-01 13:20:27 +0530

    27-10

--- app/src/main/java/com/example/legalcases/MainActivity.java
+++ app/src/main/java/com/example/legalcases/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.legalcases;
 
         import androidx.appcompat.app.AppCompatActivity;
 
+        import android.content.Intent;
         import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.EditText;
+        import android.widget.QuickContactBadge;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText e1,e2;
+    Button signIn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        e1=findViewById(R.id.name);
+        e2=findViewById(R.id.password);
+        signIn=findViewById(R.id.signin);
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i =new Intent(MainActivity.this,basicsearch.class);
+                startActivity(i);
+                //finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/legalcases/basicsearch.java
+++ app/src/main/java/com/example/legalcases/basicsearch.java
@@ -2,13 +2,35 @@ package com.example.legalcases;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class basicsearch extends AppCompatActivity {
-
+    ArrayAdapter<CharSequence> ad;
+    Spinner filterType;
+    FloatingActionButton add;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_basicsearch);
+        filterType = findViewById(R.id.filtertype);
+        ad=ArrayAdapter.createFromResource(basicsearch.this,R.array.filter, android.R.layout.simple_spinner_item);
+        ad.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        filterType.setAdapter(ad);
+
+        add=findViewById(R.id.add);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i =new Intent(basicsearch.this,editcase.class);
+                startActivity(i);
+            }
+        });
     }
 }
