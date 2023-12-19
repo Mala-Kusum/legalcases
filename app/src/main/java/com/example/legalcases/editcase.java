19895ee89fdd61f9dcefee97ac82f7f80689db05
Shreya <thisisshreya1998@gmail.com>
2023-12-19 16:49:44 +0530

    1-11

--- app/src/main/java/com/example/legalcases/editcase.java
+++ app/src/main/java/com/example/legalcases/editcase.java
@@ -10,5 +10,6 @@ public class editcase extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editcase);
+        this.getSupportActionBar().hide();
     }
 }
