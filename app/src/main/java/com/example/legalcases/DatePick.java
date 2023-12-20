ac7c932f881f0437c4d2854682967d6775e31094
Shreya <thisisshreya1998@gmail.com>
2023-12-20 18:05:34 +0530

    1-11

---
+++ app/src/main/java/com/example/legalcases/DatePick.java
@@ -0,0 +1,37 @@
+package com.example.legalcases;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import androidx.fragment.app.DialogFragment;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class DatePick extends DialogFragment implements DatePickerDialog.OnDateSetListener{
+    EditText ed;
+    Date d;
+    public DatePick(EditText ed) {
+        this.ed = ed;
+    }
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the current date as the default date in the picker
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+        // Create a new instance of DatePickerDialog and return it
+        return new DatePickerDialog(requireContext(), this, year, month, day);
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        final Calendar c = Calendar.getInstance();
+        c.set(year,month,dayOfMonth);
+        ed.setText(dayOfMonth + " - " + month + " - " + year);
+    }
+}
--- app/src/main/java/com/example/legalcases/editcase.java
+++ app/src/main/java/com/example/legalcases/editcase.java
@@ -1,15 +1,45 @@
 package com.example.legalcases;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+import java.util.Date;
 
 public class editcase extends AppCompatActivity {
-
+    EditText ed3;
+    LinearLayout nh;
+    public static Date dn;
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editcase);
         this.getSupportActionBar().hide();
+        ed3 = findViewById(R.id.nexthearingdate);
+        nh = findViewById(R.id.nh);
+
+        /*ed3.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                DialogFragment newFragment = new DatePick(ed3);
+                newFragment.show(getSupportFragmentManager(), "datePicker");
+                return false;
+            }
+        });*/
+        ed3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment newFragment = new DatePick(ed3);
+                newFragment.show(getSupportFragmentManager(), "datePicker");
+            }
+        });
     }
 }
