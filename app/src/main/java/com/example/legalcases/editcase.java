133baeaebbdff69a1b15b796da8374f44dc198b6
Shreya <thisisshreya1998@gmail.com>
2024-01-01 12:19:53 +0530

    1-11

--- app/src/main/java/com/example/legalcases/editcase.java
+++ app/src/main/java/com/example/legalcases/editcase.java
@@ -18,9 +18,9 @@ public class editcase extends AppCompatActivity {
     EditText ed3,ed4,ed16;
     LinearLayout nh;
     public static Date dn;
-    ArrayAdapter<String> ad1,ad2,ad3;
-    AutoCompleteTextView a1,a2,a3;
-    @SuppressLint("ClickableViewAccessibility")
+    ArrayAdapter<String> ad1,ad2,ad3,ad4;
+    AutoCompleteTextView a1,a2,a3,a4;
+    @SuppressLint({"ClickableViewAccessibility", "ResourceType"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,9 +34,11 @@ public class editcase extends AppCompatActivity {
         a1 = (AutoCompleteTextView) findViewById(R.id.statushead);
         a2 = (AutoCompleteTextView) findViewById(R.id.hqro);
         a3 = (AutoCompleteTextView) findViewById(R.id.state);
+        a4 = (AutoCompleteTextView) findViewById(R.id.courtType);
         String[] s1 = {"Closed- Won","Closed- Lost","Ongoing","Settled","Disposed","Dismissed","Sine Dine"};
         String[] s2 = {"HQ","RO"};
-
+        String[] s3 = {"HQ","Tripura","Uttarakhand","Nagaland","Sikkim","Ladakh","Andaman","Manipur","Meghalaya","Assam","Mizoram","J&K","Mizoram","Arunachal Pradesh","Andaman and Nicobar","Punjab","Haryana","West Bengal"};
+        String[] s4 = {"Supreme Court","District Court","High Court","Civil Court","NGT","Arbitration","Arbitration-LA","NCLT","Others"};
         //defining all the autocompletextview
         ad1 = new ArrayAdapter<String>(this,android.R.layout.select_dialog_singlechoice, s1);
         a1.setThreshold(1);
@@ -47,17 +49,16 @@ public class editcase extends AppCompatActivity {
                 a1.showDropDown();
             }
         });
-
         ad2 = new ArrayAdapter<String>(this,android.R.layout.select_dialog_singlechoice, s2);
-        a1.setThreshold(1);
-        a1.setAdapter(ad2);
+        a2.setThreshold(1);
+        a2.setAdapter(ad2);
         a2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 a2.showDropDown();
             }
         });
-        ad2 = new ArrayAdapter<String>(this,android.R.layout.select_dialog_singlechoice, R.array.States);
+        ad3 = new ArrayAdapter<String>(this,android.R.layout.select_dialog_singlechoice, s3);
 
         a3.setThreshold(1);
         a3.setAdapter(ad3);
@@ -67,6 +68,17 @@ public class editcase extends AppCompatActivity {
                 a3.showDropDown();
             }
         });
+
+        ad4 = new ArrayAdapter<String>(this,android.R.layout.select_dialog_singlechoice, s4);
+        a4.setThreshold(1);
+        a4.setAdapter(ad4);
+        a4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                a4.showDropDown();
+            }
+        });
+
         ed3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
